7fae7c6c128571d7d25aa5a6d8967b6bc30f06a3
EDGE775 <48449257+EDGE775@users.noreply.github.com>
2020-11-25 22:52:08 +0300

    5.5. Простое число [#405266] Изменил условие перед циклом

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,13 +2,12 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = number == 2; // Добавил начальное условие для числа 2, т.к. оно не попадает в цикл
+        boolean prime = number > 1; // Изменил условие
         for (int i = 2; i < number; i++) {
             if (number % i == 0) {
                 prime = false; // Добавил присвоение значения false внутри оператора условия
                 break;
             }
-            prime = true;
         }
         return prime;
     }
